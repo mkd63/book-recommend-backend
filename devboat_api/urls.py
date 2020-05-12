82fff5f731ff88847e7f039774dce0a8f780eb52
Sparsh Tyagi <sparshtyagi21@gmail.com>
2020-05-12 14:33:12 +0200

    revert plural urls

--- devboat_api/urls.py
+++ devboat_api/urls.py
@@ -19,9 +19,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('users.urls')),
-    path('api/v1/',include('educations.urls')),
+    path('api/v1/',include('education.urls')),
     path('api/v1/',include('interests.urls')),
-    path('api/v1/',include('jobs.urls')),
+    path('api/v1/',include('job.urls')),
     path('api/v1/',include('projects.urls')),
     path('api/v1/',include('skills.urls')),
     path('api-auth/',include('rest_framework.urls')),
