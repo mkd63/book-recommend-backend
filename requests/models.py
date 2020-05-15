dc16d1f8a0078c9c5f41812c682b5f5662e273f8
Sparsh Tyagi <sparshtyagi21@gmail.com>
2020-05-16 01:43:47 +0200

    add requests unique set

--- requests/models.py
+++ requests/models.py
@@ -6,5 +6,8 @@ class Requests(models.Model):
     receiver = models.ForeignKey(Users, related_name="receiver", null=True, on_delete=models.SET_NULL)
     request_status = models.IntegerField(null=True,default=0)
 
+    class Meta:
+        unique_together = ["sender", "receiver"]
+
     def __str__(self):
         return self.name
