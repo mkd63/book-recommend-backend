801df0bdba17072bed998707e7ff1cc19dbbc914
Sparsh Tyagi <sparshtyagi21@gmail.com>
2020-05-16 01:44:00 +0200

    add connections unique set

--- connections/models.py
+++ connections/models.py
@@ -6,5 +6,8 @@ class Connections(models.Model):
     user1 = models.ForeignKey(Users, related_name="user1", null=True, on_delete=models.SET_NULL)
     user2 = models.ForeignKey(Users, related_name="user2", null=True, on_delete=models.SET_NULL)
 
+    class Meta:
+        unique_together = ["user1", "user2"]
+
     def __str__(self):
         return self.name
