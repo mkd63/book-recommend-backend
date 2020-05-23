6240ba389713783f017cb4f017928dd8b348e4b5
Sparsh Tyagi <sparshtyagi21@gmail.com>
2020-05-23 17:59:12 +0200

    add created_on to interests

--- interests/models.py
+++ interests/models.py
@@ -4,3 +4,7 @@ from users.models import Users
 class Interests(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(Users, on_delete = models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
