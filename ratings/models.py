15ed83a49a7d9c75688b362b4d4b35e29c74cec8
mkd63 <sharmaaahan15@gmail.com>
2021-03-31 00:25:44 +0530

    SET_NULL to CASCADE in ratings table

--- ratings/models.py
+++ ratings/models.py
@@ -3,8 +3,8 @@ from users.models import Users
 from books.models import Books
 
 class Ratings(models.Model):
-    user = models.ForeignKey(Users, related_name="user", null=True, on_delete=models.SET_NULL)
-    book = models.ForeignKey(Books, related_name="book", null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(Users, related_name="user", null=True, on_delete=models.CASCADE)
+    book = models.ForeignKey(Books, related_name="book", null=True, on_delete=models.CASCADE)
     rating = models.FloatField()
     created_on = models.DateTimeField(auto_now_add=True)
 
